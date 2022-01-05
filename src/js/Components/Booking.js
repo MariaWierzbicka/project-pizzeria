6e7413dc2ad5b0472f259102b3377890bfd19677
Maria Wierzbicka <maria.a.wierzbicka@gmail.com>
2022-01-05 10:27:42 +0100

    Add booking widgets with plugins

--- src/js/Components/Booking.js
+++ src/js/Components/Booking.js
@@ -1,6 +1,8 @@
 import {select, templates} from '../settings.js';
 import {utils} from '../utils.js';
 import AmountWidget from './AmountWidget.js';
+import DatePicker from './DatePicker.js';
+import HourPicker from './HourPicker.js';
 
 
 class Booking{
@@ -22,6 +24,9 @@ class Booking{
 
     thisBooking.dom.peopleAmount = document.querySelector(select.booking.peopleAmount);
     thisBooking.dom.hoursAmount = document.querySelector(select.booking.hoursAmount);
+    thisBooking.dom.datePicker = document.querySelector(select.widgets.datePicker.wrapper);
+    thisBooking.dom.hourPicker = document.querySelector(select.widgets.hourPicker.wrapper);
+
   }
   initWidgets(){
     const thisBooking = this;
@@ -37,6 +42,10 @@ class Booking{
     thisBooking.dom.hoursAmount.addEventListener('click', function(){
 
     });
+
+    thisBooking.datePickerWidget = new DatePicker(thisBooking.dom.datePicker);
+
+    thisBooking.hourPickerWidget = new HourPicker(thisBooking.dom.hourPicker);
   }
 }
 export default Booking;
