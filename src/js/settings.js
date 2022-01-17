c3352cb9720da114261c84123a966694abd70c5d
Maria Wierzbicka <maria.a.wierzbicka@gmail.com>
2022-01-17 18:26:06 +0100

    Remove unused lines

--- src/js/settings.js
+++ src/js/settings.js
@@ -126,7 +126,7 @@ export const settings = {
     images: 'images',
     products: 'products',
     orders: 'orders',
-    url: '//localhost:3131',
+    url: '//' + window.location.hostname + (window.location.hostname=='localhost' ? ':3131' : ''),
     product: 'product',
     order: 'order',
     bookings: 'bookings',
